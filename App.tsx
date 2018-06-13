1a0fd451b97ec3d777fa197f9d469effd17d0fb1
Rajesh Kumar <rajesh.kumar@causecode.com>
2018-06-13 18:30:14 +0530

    changing back to working code

--- App.tsx
+++ App.tsx
@@ -1,62 +1,60 @@
 import * as React from "react";
 import { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import Bmi from "./components/bmi";
 import { Tabs } from "./components/config/router";
 import Header from "./components/Header";
-import * as data from "./customData.json";
-
-export interface IPinPageState {
-  data: any;
-  editing: boolean;
-}
 
 // const word: any = (data).name;
 
 const instructions = Platform.select({
-  android: "Double tap R on your keyboard to reload,\n" +
+  android:
+    "Double tap R on your keyboard to reload,\n" +
     "Shake or press menu button for dev menu",
-  ios: "Press Cmd+R to reload,\n" +
-    "Cmd+D or shake for dev menu",
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
 });
 
-export default class App extends Component<any, IPinPageState> {
+export default class App extends Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-     data: {
-      calories: 37,
-      climbed: 0,
-      distance: 1050,
-      heartRate: 72,
-      height: 1.7,
-      steps: 1505,
-      weight: 64,
-     },
-     editing: false,
+      data: {
+        calories: 37,
+        climbed: 0,
+        distance: 1050,
+        heartRate: 72,
+        height: 1.7,
+        steps: 1505,
+        weight: 64,
+       },
+       editing: false,
     };
-}
-
-  public onEdit = () =>
-  this.setState({editing : !this.state.editing})
-
-  public render() {
-    return (
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.body}>
-        <Tabs screenProps={{ data: this.state.data , editing: this.state.editing}} />
-        </View>
-        <Bmi data={this.state.data} handleClick={this.onEdit.bind(this)} />
-      </View>
-    );
   }
 
+  onSubmit = () => {
+    this.setState({
+      editing : !this.state.editing,
+    });
+  }
+  public render() {
+    return (
+       <View style={styles.container}>
+              <Header />
+              <View style={styles.body}>
+                <Tabs
+                  screenProps={{
+                    data: this.state.data,
+                    editing: this.state.editing,
+                  }}
+                />
+              </View>
+              <Bmi
+                data={this.state.data}
+                handleClick={this.onSubmit.bind(this)}
+              />
+            </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
